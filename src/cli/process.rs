395af1b8ea7355fd847d8c602fed5cc73adb25dc
cpprian <cpprian456@gmail.com>
2024-03-13 21:47:01 +0100

    delete lifetime tag

--- src/cli/process.rs
+++ src/cli/process.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 
 pub use crate::cli::structs::CliHandler;
 
-pub fn process_cli_data<'a>(writer: &mut impl std::io::Write) -> Result<CliHandler, anyhow::Error> {
+pub fn process_cli_data(writer: &mut impl std::io::Write) -> Result<CliHandler, anyhow::Error> {
     let cli = CliHandler::parse();
 
     // TODO: consider add this message as a log
